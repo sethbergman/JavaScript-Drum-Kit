460b3c0efbf0f5401e273513ebf6e8e5d864960c
Seth Bergman <seth.atxwebs@gmail.com>
2019-08-16 12:32:11 -0500

    Refactored for ES6

--- README.md
+++ README.md
@@ -1,29 +1,34 @@
 # JavaScript Drum Kit
 
-##### Play the drums on your keyboard
+> Play the drums on your keyboard
 
 [![Build Status](https://travis-ci.org/sethbergman/JavaScript-Drum-Kit.svg?branch=master)](https://travis-ci.org/sethbergman/JavaScript-Drum-Kit)
 
 [![Greenkeeper badge](https://badges.greenkeeper.io/sethbergman/JavaScript-Drum-Kit.svg)](https://greenkeeper.io/)
 
----
+### Clone the repository
+
+```bash
+# clone and change directory
+git clone https://github.com/sethbergman/JavaScript-Drum-Kit.git && cd JavaScript-Drum-Kit
+```
 
 ### Install dependencies
 
-```sh
-npm i # or use yarn
+```bash
+npm i
+# or use yarn
 yarn
 ```
 
 ### Start the application
 
-```sh
-npm start # or
+```bash
+npm start
+# or
 yarn start
 ```
 
 ### Jam out :smiley:
 
-Navigate to http://localhost:5000 and have fun!
-
-#### More documentation coming soon :octocat:
+Navigate to [localhost:5000](http://localhost:5000) and have fun!
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const errorHandler = require('errorhandler')
+const path = require('path')
 const app = express()
 
 app.engine('html', require('ejs').renderFile)
-app.set('/views', express.static(__dirname + '/views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'html')
-app.use('/assets', express.static(__dirname + '/assets'))
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
-
-app.get('/', function (request, response) {
-	response.render('index')
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
+app.use(errorHandler())
 
-const server = app.listen(process.env.PORT || 5000, function () {
-	console.info('Server running at http://0.0.0.0:' + server.address().port)
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Server running at http://localhost:' + server.address().port)
 })
+
+module.exports = server
